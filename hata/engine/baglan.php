7fb87c973891ae025cc31d3f53a47076d1df07e7
sedat-unal <44405778+sedat-unal@users.noreply.github.com>
2020-10-25 16:33:58 +0300

    first commit

---
+++ hata/engine/baglan.php
@@ -0,0 +1,34 @@
+<?php
+    $host = "localhost";
+    $dbname = "rfyalmhi_blog";
+    $user = "rfyalmhi_blog";
+    $pass = "FNaT%-@0g23L";
+    // "5.2.87.161"
+    try{
+        $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8;", "$user", "$pass");
+        $db->query("SET CHARSET SET UTF8");
+        $db->query("SET NAMES UTF8");
+    }catch (PDOException $hata){
+        echo $hata->getMessage();
+    }
+
+
+
+    ## Ayarlar tablosu bağlantısı ##
+    $ayarlar = $db->prepare("SELECT * FROM ayarlar");
+    $ayarlar ->execute();
+    $arow       = $ayarlar->fetch(PDO::FETCH_OBJ);
+    $site       = $arow->site_url;
+    $logo       = $arow->site_icon;
+    $sitekeyw   = $arow->site_keyw;
+    $sitedesc   = $arow->site_desc;
+    $sitebaslik = $arow->site_baslik;
+    ##
+
+
+
+    ## Hakkımda tablosu bağlantısı ##
+    $hakkimda = $db->prepare("SELECT * FROM hakkimda");
+    $hakkimda ->execute();
+    $hrow     = $hakkimda->fetch(PDO::FETCH_OBJ);
+    ##
